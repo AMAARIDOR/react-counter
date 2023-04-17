ace2f4c9a5dcbcd32d7d5c9585dbf59f0a0e71a3
Amaari <arish2k3@gmail.com>
2023-04-17 18:29:30 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  let [count, setCount] = useState(0);
+
+  function handleClickDecrement() {
+    setCount(count - 1);
+  }
+
+  function handleClickIncrement() {
+    setCount(count + 1);
+  }
+
+  function resetCounter() {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <button onClick={handleClickDecrement}>-</button>
+      <div>{count}</div>
+      <button onClick={handleClickIncrement}>+</button>
+      <button onClick={resetCounter}>Reset Counter</button>
+    </>
+  );
 }
 
 export default App;
